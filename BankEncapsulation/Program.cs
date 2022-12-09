2ffdd6b363d145e74b32e24cbc8b61072b32e057
DeAdrien Hill <deadrienhill@yahoo.com>
2022-12-09 12:30:58 -0600

    completed encapsulation

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -1,10 +1,44 @@
-﻿namespace BankEncapsulation
+﻿using System;
+
+namespace BankEncapsulation
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+
+
+            BankAccount bankAccount = new();
+            {
+                Console.WriteLine($"Please enter deposit amount?");
+
+                var amountToDep = double.Parse(Console.ReadLine());
+
+
+                bankAccount.Deposit(amountToDep);
+
+                Console.WriteLine($"You're new balance is {bankAccount.GetBalance()}");
+
+
+
+               
+
+                
+
+
+
+
+
+
+
+
+
+            }
+
+
+
+
         }
     }
 }
